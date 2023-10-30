d9b6b4a3d820f872c316533ee7ebf8a6edbe3a23
sunderdevops143 <66628090+sunderdevops143@users.noreply.github.com>
2023-10-30 17:13:14 +0000

    Technical-Test---QuantSpark files

---
+++ ec2-resources.tf
@@ -0,0 +1,59 @@
+# Public subnet EC2 instance 1
+resource "aws_instance" "two-tier-web-server-1" {
+  ami             = "ami-064eb0bee0c5402c5"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.two-tier-ec2-sg.id]
+  subnet_id       = aws_subnet.two-tier-pub-sub-1.id
+
+  tags = {
+    Name = "two-tier-web-server-1"
+  }
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y 
+sudo systemctl enable nginx
+sudo systemctl start nginx
+# Add custom data
+echo "QUANTSPARK world" > /usr/share/nginx/html/index.html
+EOF
+}
+
+# Public subnet  EC2 instance 2
+resource "aws_instance" "two-tier-web-server-2" {
+  ami             = "ami-064eb0bee0c5402c5"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.two-tier-ec2-sg.id]
+  subnet_id       = aws_subnet.two-tier-pub-sub-2.id
+
+  tags = {
+    Name = "two-tier-web-server-2"
+  }
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y 
+sudo systemctl enable nginx
+sudo systemctl start nginx
+# Add custom data
+echo "QUANTSPARK world" > /usr/share/nginx/html/index.html
+EOF
+}
+
+#EIP
+
+resource "aws_eip" "two-tier-web-server-1-eip" {
+  vpc = true
+
+  instance                  = aws_instance.two-tier-web-server-1.id
+  depends_on                = [aws_internet_gateway.two-tier-igw]
+}
+
+resource "aws_eip" "two-tier-web-server-2-eip" {
+  vpc = true
+
+  instance                  = aws_instance.two-tier-web-server-2.id
+  depends_on                = [aws_internet_gateway.two-tier-igw]
+}
